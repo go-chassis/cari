3b6ea34853043221a66077e024de6d92d58ef363
robotljw <790504160@qq.com>
2021-11-25 10:43:13 +0800

    Add task and tombstone struct for sync

---
+++ sync/types.go
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sync
+
+// Task is db struct to store sync task
+type Task struct {
+	TaskID    string      `json:"task_id" bson:"task_id"`
+	Action    string      `json:"action" bson:"action"`
+	DataType  string      `json:"data_type" bson:"data_type"`
+	Data      interface{} `json:"data" bson:"data"`
+	Timestamp int64       `json:"timestamp" bson:"timestamp"`
+	Status    string      `json:"status" bson:"status"`
+}
+
+// Tombstone is db struct to store the deleted resource information
+type Tombstone struct {
+	ResourceID   string `json:"resource_id" bson:"resource_id"`
+	ResourceType string `json:"resource_type" bson:"resource_type"`
+	Domain       string `json:"domain" bson:"domain"`
+	Project      string `json:"project" bson:"project"`
+	Timestamp    int64  `json:"timestamp" bson:"timestamp"`
+}
